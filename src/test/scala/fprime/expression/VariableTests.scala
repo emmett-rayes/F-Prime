b14036b673e8514c5110f4448fe91bcec84aa423
Emmett Rayes <emmett.rayes@icloud.com>
2024-05-05 10:23:19 +0200

    Add variable parser tests

---
+++ src/test/scala/fprime/expression/VariableTests.scala
@@ -0,0 +1,21 @@
+package fprime.expression
+
+import fprime.parsing.{asTokens, summonParser}
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers.{matchPattern, should}
+
+import scala.util.{Failure, Success}
+
+class VariableTests extends AnyFunSuite {
+    test("variable parser (green)") {
+        val input = "x".asTokens
+        summonParser[Variable].parse(input) should matchPattern {
+            case Success((_, Variable(symbol))) if symbol == "x" =>
+        }
+    }
+
+    test("variable parser (red)") {
+        val input = "->".asTokens
+        summonParser[Variable].parse(input) should matchPattern { case Failure(_) => }
+    }
+}
