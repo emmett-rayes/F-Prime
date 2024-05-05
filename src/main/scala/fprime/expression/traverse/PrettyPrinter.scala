2119a36e93ddeffd61d048befede9da1fb3d1c58
Emmett Rayes <emmett.rayes@icloud.com>
2024-05-05 13:49:11 +0200

    Add initial pretty printer

---
+++ src/main/scala/fprime/expression/traverse/PrettyPrinter.scala
@@ -0,0 +1,23 @@
+package fprime.expression.traverse
+
+import fprime.expression.{Abstraction, Application, Expression, Variable, given}
+
+import scala.PartialFunction.cond
+
+object PrettyPrinter:
+    def pretty(expression: Expression): String = expression match
+        case Variable(symbol) => symbol
+
+        case Abstraction(parameter, body) =>
+            val bodyIsAbstraction = cond(body) { case Abstraction(_, _) => true }
+            val prettyParameter = pretty(parameter)
+            var prettyBody = pretty(body)
+            if bodyIsAbstraction then prettyBody = prettyBody.stripPrefix("(").stripSuffix(")")
+            s"(λ$prettyParameter. $prettyBody)"
+
+        case Application(callable, argument) =>
+            val argumentIsApplication = cond(argument) { case Application(_, _) => true }
+            val prettyCallable = pretty(callable)
+            var prettyArgument = pretty(argument)
+            if argumentIsApplication then prettyArgument = s"($prettyArgument)"
+            s"$prettyCallable $prettyArgument"
