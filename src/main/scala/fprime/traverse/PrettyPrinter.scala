743bc953e7ba1ea74297ffed572a9b74b1f736df
Emmett Rayes <emmett.rayes@icloud.com>
2024-05-06 17:40:01 +0200

    Extend pretty printer modes

--- src/main/scala/fprime/traverse/PrettyPrinter.scala
+++ src/main/scala/fprime/traverse/PrettyPrinter.scala
@@ -1,23 +1,49 @@
 package fprime.traverse
 
 import fprime.expression.{Abstraction, Application, Expression, Variable}
+import fprime.traverse.DeBruijnVariable
 
 import scala.PartialFunction.cond
 
 object PrettyPrinter:
-    def pretty(expression: Expression): String = expression match
-        case Variable(symbol) => symbol
+    enum Mode:
+        case Named
+        case Indexed
+        case NamelessLocals
 
-        case Abstraction(parameter, body) =>
-            val bodyIsAbstraction = cond(body) { case Abstraction(_, _) => true }
-            val prettyParameter = pretty(parameter)
-            var prettyBody = pretty(body)
-            if bodyIsAbstraction then prettyBody = prettyBody.stripPrefix("(").stripSuffix(")")
-            s"(λ$prettyParameter. $prettyBody)"
+    private def pretty(expression: Expression, scope: Int)(using mode: Mode): String =
+        expression match
+            case variable @ Variable(symbol) =>
+                mode match
+                    case Mode.Named => variable.symbol
+                    case _ =>
+                        variable match
+                            case deBruijn: DeBruijnVariable =>
+                                mode match
+                                    case Mode.Named   => throw RuntimeException("unreachable!")
+                                    case Mode.Indexed => deBruijn.index.toString
+                                    case Mode.NamelessLocals =>
+                                        if deBruijn.index <= scope then deBruijn.index.toString
+                                        else variable.symbol
+                            case _ => symbol
 
-        case Application(callable, argument) =>
-            val argumentIsApplication = cond(argument) { case Application(_, _) => true }
-            val prettyCallable = pretty(callable)
-            var prettyArgument = pretty(argument)
-            if argumentIsApplication then prettyArgument = s"($prettyArgument)"
-            s"$prettyCallable $prettyArgument"
+            case Abstraction(parameter, body) =>
+                val bodyIsAbstraction = cond(body) { case Abstraction(_, _) => true }
+                val prettyParameter = pretty(parameter, scope)
+                var prettyBody = pretty(body, scope)
+                if bodyIsAbstraction then
+                    prettyBody = prettyBody.stripPrefix("(").stripSuffix(")")
+                mode match
+                    case Mode.Named => s"(λ$prettyParameter. $prettyBody)"
+                    case Mode.Indexed | Mode.NamelessLocals => s"(λ $prettyBody)"
+
+            case Application(callable, argument) =>
+                val argumentIsApplication = cond(argument) { case Application(_, _) => true }
+                val prettyCallable = pretty(callable, scope)
+                var prettyArgument = pretty(argument, scope)
+                if argumentIsApplication then prettyArgument = s"($prettyArgument)"
+                s"$prettyCallable $prettyArgument"
+
+    def pretty(expression: Expression, mode: Mode = Mode.Named): String =
+        given Mode = mode
+        pretty(expression, 0)
