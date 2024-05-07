4e7cf89be213d9ffe7a0277fc819cc8e3f1f21ad
Emmett Rayes <emmett.rayes@icloud.com>
2024-05-07 21:55:55 +0200

    Add call by value evaluator test

---
+++ src/test/scala/fprime/traverse/CallByValueEvaluatorTests.scala
@@ -0,0 +1,14 @@
+package fprime.traverse
+
+import fprime.util.parseTerm
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers.{matchPattern, should}
+
+class CallByValueEvaluatorTests extends AnyFunSuite:
+    test("full beta") {
+        val term = parseTerm("(λn.λs.λz.s (n s z)) (λs.λz.z)")
+        val converted = DeBruijnConverter.convert(term)
+        val reduced = CallByValueEvaluator.reduce(converted, normalize = true).get
+        val pretty = PrettyPrinter.pretty(reduced, mode = PrettyPrinter.Mode.Named)
+        assert(pretty == "λs. λz. s z")
+    }
