99e19b8b9a032c07a66912d493c3b584e15d12cd
Emmett Rayes <emmett.rayes@icloud.com>
2024-05-20 19:25:12 +0200

    Improve parse error handling in REPL

--- src/main/scala/fprime/repl/REPL.scala
+++ src/main/scala/fprime/repl/REPL.scala
@@ -1,40 +1,44 @@
 package fprime.repl
 
-import fprime.parser.result
-import fprime.parsing.summonParser
+import fprime.parsing.{skipWhitespace, summonParser}
 import fprime.traverse.{CallByValueEvaluator, DeBruijnConverter, PrettyPrinter}
 import fprime.untyped.UntypedTerm
 import fprime.untyped.UntypedTerm.UntypedTermParser
 
 import scala.io.StdIn.readLine
+import scala.util.control.Breaks.{break, breakable}
 import scala.util.{Failure, Success}
 
 private def prompt(): String =
     print(">> ")
     readLine()
 
-private def error(): Unit = println("!!")
+private def error(): Unit =
+    println("!!")
+    break
 
 @main
 def repl(): Nothing =
+    val parser = summonParser[UntypedTerm]
     while true do
-        val input = prompt()
-
-        val parser = summonParser[UntypedTerm]
-        val trace = for
-            term <- parser.parse(input).result
-            pretty = PrettyPrinter.pretty(term)
-            converted = DeBruijnConverter.convert(term)
-        yield CallByValueEvaluator.reduce(converted, normalize = true)
-
-        trace match
-            case Failure(exception) => error()
-            case Success(trace) =>
-                if trace.isEmpty then println("stuck!")
-                else
-                    trace.zipWithIndex.foreach((term, i) =>
-                        val pretty = PrettyPrinter.pretty(term)
-                        println(s"$i. $pretty")
-                    )
+        breakable {
+            val input = prompt()
+            val trace = for
+                (remaining, term) <- parser.parse(input)
+                _ = if remaining.skipWhitespace.nonEmpty then error()
+                pretty = PrettyPrinter.pretty(term)
+                converted = DeBruijnConverter.convert(term)
+            yield CallByValueEvaluator.reduce(converted, normalize = true)
+            trace match
+                case Failure(exception) => error()
+                case Success(trace) =>
+                    if trace.isEmpty then println("stuck!")
+                    if trace.size == 1 then println(PrettyPrinter.pretty(trace.head))
+                    else
+                        trace.zipWithIndex.foreach((term, i) =>
+                            val pretty = PrettyPrinter.pretty(term)
+                            println(s"$i. $pretty")
+                        )
+        }
 
     throw RuntimeException("unreachable!")
