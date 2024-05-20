105749779c15d290c1f8c5cab608e2171bbfa520
Emmett Rayes <emmett.rayes@icloud.com>
2024-05-20 21:57:54 +0200

    Finish simply typed term definition

--- src/main/scala/fprime/simple/SimplyTypedTerm.scala
+++ src/main/scala/fprime/simple/SimplyTypedTerm.scala
@@ -1,27 +1,27 @@
 package fprime.simple
 
-import fprime.expression.*
+import fprime.expression.{*, given}
 import fprime.parser.Parser
 import fprime.parser.combinators.map
 import fprime.parsing.{Parsable, Tokens, summonParser}
+import fprime.simple.SimpleType.SimpleTypeParser
 
-private type SimpleType = Variable
-
-private type SimplyTypedVariableInner = Ascription[Variable, SimpleType]
-private type SimplyTypedAbstractionInner = Abstraction[SimplyTypedVariable, SimplyTypedTerm]
+private type SimplyTypedVariableInner = Variable[SimplyTypedTerm]
+private type SimplyTypedAbstractionInner =
+    Abstraction[Ascription[Variable[SimplyTypedTerm], SimpleType], SimplyTypedTerm]
 private type SimplyTypedApplicationInner = Application[SimplyTypedTerm, SimplyTypedTerm]
 
-type SimplyTypedTerm = Expression &
-    (SimplyTypedVariable | SimplyTypedAbstraction | SimplyTypedApplication)
+type SimplyTypedTerm = SimplyTypedVariable | SimplyTypedAbstraction | SimplyTypedApplication
 
-class SimplyTypedVariable(variable: Variable, `type`: SimpleType)
-    extends SimplyTypedVariableInner(variable, `type`)
+class SimplyTypedVariable(symbol: Symbol) extends SimplyTypedVariableInner(symbol)
 
 object SimplyTypedVariable:
     def unapply(variable: SimplyTypedVariable): SimplyTypedVariableInner = variable
 
-class SimplyTypedAbstraction(parameter: SimplyTypedVariable, body: SimplyTypedTerm)
-    extends SimplyTypedAbstractionInner(parameter, body)
+class SimplyTypedAbstraction(
+    parameter: Ascription[Variable[SimplyTypedTerm], SimpleType],
+    body: SimplyTypedTerm,
+) extends SimplyTypedAbstractionInner(parameter, body)
 
 object SimplyTypedAbstraction:
     def unapply(abstraction: SimplyTypedAbstraction): SimplyTypedAbstractionInner = abstraction
@@ -34,7 +34,7 @@ object SimplyTypedApplication:
 
 object SimplyTypedTerm:
     private given Conversion[SimplyTypedVariableInner, SimplyTypedVariable] =
-        ascription => SimplyTypedVariable(ascription.expression, ascription.`type`)
+        variable => SimplyTypedVariable(variable.symbol)
 
     private given Conversion[SimplyTypedAbstractionInner, SimplyTypedAbstraction] =
         abstraction => SimplyTypedAbstraction(abstraction.parameter, abstraction.body)
@@ -45,10 +45,10 @@ object SimplyTypedTerm:
     given SimplyTypedTermParser: Parsable[SimplyTypedTerm] with
         override lazy val parser: Parser[Tokens, SimplyTypedTerm] =
             // cast safety: the correct parser was supplied by the local given
+            // match safety: a default parser that fails early is always defined
             summonParser[Expression].map {
                 case e: SimplyTypedVariableInner @unchecked    => e
                 case e: SimplyTypedAbstractionInner @unchecked => e
                 case e: SimplyTypedApplicationInner @unchecked => e
-                case _ => throw RuntimeException("unreachable")
-                // match safety: a default parser that fails early is always defined
+                case _                                         => throw Exception("unreachable")
             }
--- src/test/scala/fprime/simple/SimplyTypedTermTests.scala
+++ src/test/scala/fprime/simple/SimplyTypedTermTests.scala
@@ -1,15 +1,30 @@
 package fprime.simple
 
+import fprime.expression.*
 import fprime.simple.SimplyTypedTerm.SimplyTypedTermParser
 import fprime.util.parse
+import org.scalatest.Inside.inside
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers.{matchPattern, should}
 
 class SimplyTypedTermTests extends AnyFunSuite:
     test("simply typed variable") {
-        val term = parse[SimplyTypedTerm]("x : Int")
-        term should matchPattern {
-            case SimplyTypedVariable(variable, t)
-                if variable.symbol == "x" && t.symbol == "Int" =>
+        val term = parse[SimplyTypedTerm]("λx: => A A.x")
+        inside(term) { case SimplyTypedAbstraction(parameters, body) =>
+            inside(parameters) { case Ascription(Variable(symbol, _), _type) =>
+                assert(symbol == "x")
+                inside(_type) {
+                    case SimpleFunctionType(Application(_, source), target) =>
+                        inside(source) {
+                            case SimpleUninterpretedType(constant) => constant == "A"
+                        }
+                        inside(target) {
+                            case SimpleUninterpretedType(constant) => constant == "A"
+                        }
+                }
+            }
+            inside(body) {
+                case SimplyTypedVariable(symbol, _) => assert(symbol == "x")
+            }
         }
     }
